4091aad765848e8b99c716b32d466c1d6bba6f6f
WaraWiriTravel <145569382+WaraWiriTravel@users.noreply.github.com>
2023-09-22 19:44:40 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// ini adalah Backend
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(3000, function () {
+  console.log("Server berjalan di http://localhost:3000");
+});
